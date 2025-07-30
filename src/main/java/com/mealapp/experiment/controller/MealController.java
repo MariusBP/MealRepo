038fb1fe812be86392bd705eee6d58ea2c49c321
Marius B. Pran <31306299+MariusBP@users.noreply.github.com>
2025-07-30 12:31:27 +0200

    Created category.yaml (#26)
    * - added category.yaml
    - added two api calls to category.yaml. /categories to get a list of all categories. /category post to create new categories
    
    * - removed unneeded Category common schema
    - fixed error bug with errors 400 and 500
    
    * #14 started working on meal functionality.
    
    * - removed unneeded "required" fields
    - fixed BadRequest error description
    - Updated Category description
    - removed unneeded examples
    - updated picture and description example strings
    
    * #14 Updating contract details, and working on meal controller.
    
    * - renamed diets.yaml to diet.yaml to be more consistent
    - added tags to category and diet
    -added generate-meal-api and generate-category-api to pom.xml
    
    * #14 endpoint now working
    
    * - renamed diets.yaml to diet.yaml to be more consistent with other files
    
    * - Fixed file structure so the static files for category, meals and diet are found by maven
    
    ---------
    
    Co-authored-by: Cooljean95 <Cooljean95@ymail.com>

---
+++ src/main/java/com/mealapp/experiment/controller/MealController.java
@@ -0,0 +1,49 @@
+package com.mealapp.experiment.controller;
+
+
+import com.mealapp.openapi.meal.api.MealApi;
+import com.mealapp.openapi.meal.model.ListMealResponse;
+import com.mealapp.openapi.meal.model.ReadMealResponse;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@AllArgsConstructor
+public class MealController implements MealApi {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("MealController initialized");
+    }
+
+    @Override
+    public ResponseEntity<ReadMealResponse> getMeal(
+            String accept,
+            Integer id,
+            String contentType,
+            String xRequestID,
+            String userAgent) {
+
+        System.out.println("getMeal called with id: " + id);
+        ReadMealResponse response = new ReadMealResponse();
+        return ResponseEntity.ok(response);
+    }
+
+    @Override
+    public ResponseEntity<List<ListMealResponse>> listMeals(
+            String accept,
+            Integer dietId,
+            List<Integer> categoryIdList,
+            String contentType,
+            String xRequestID,
+            String userAgent) {
+
+        System.out.println("listMeals called with dietId: " + dietId + ", categoryIdList: " + categoryIdList);
+        return ResponseEntity.ok(List.of());
+    }
+}
--- src/main/java/com/mealapp/experiment/model/Diet.java
+++ src/main/java/com/mealapp/experiment/model/Diet.java
@@ -1,4 +1,16 @@
 package com.mealapp.experiment.model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Entity
+@Table(name = "diet")
 public class Diet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
